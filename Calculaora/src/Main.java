d0b7db92b1a1e48192c15ffa85573efa5ac09549
gerardo <6erar8eltran@gmail.com>
2016-06-21 12:57:50 +0200

    Calculator 1.0

--- Calculaora/src/Main.java
+++ Calculaora/src/Main.java
@@ -10,6 +10,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// instancias
+		UI u = new UI();
 		Calculant q = new Calculant();
 		// variables
 		int x, y, z;
@@ -33,24 +34,18 @@ public class Main {
 			 * 1er: Controlar que has afegit algo i que no has ixit del programa amb l variable auxiliar (aux)
 			 */
 			try {
-				aux = JOptionPane.showInputDialog(null, m);
+				aux = u.input(m);
 			} catch (Exception e) {
-				//Li donem un missatge al user.
-				JOptionPane.showMessageDialog(null, "Adeu!");
-				//Li donem un altre al programador per a saber què ha passat.
-				System.err.println(e.getMessage());
-				System.exit(0);
+				u.error("Adeu!", "Estas ixint de la app!", e);
 			}
 			/**
 			 * 2on: Una vegada que la variable auxiliar no es null, la convertirem en un nombre.
 			 */
 			try {
-				x = Integer.parseInt(aux);
+				x = u.integer(aux);
 			} catch (NumberFormatException nfe) {
 				//En el cas de que no hages un ficat un nombre entrarà en aqusta excepció i traurà el missatge de error.
-				JOptionPane.showMessageDialog(null, "Tan sols es poden calcular nombres!", "Error", JOptionPane.ERROR_MESSAGE);
-				System.err.println(nfe.getMessage());
-				System.exit(0);
+				u.error("HAs de posar un num!", nfe);
 			}
 			//Despres no oblides inicialitzar la variable aux SEMPRE.
 			aux = "";
@@ -58,30 +53,23 @@ public class Main {
 			
 			String[] options = new String[] { "+", "-", "*", "/" };
 			try{
-				z = JOptionPane.showOptionDialog(null, "Introdueix el teu operador.", "Calcula", JOptionPane.DEFAULT_OPTION,
-						JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+				z = u.option("Introdueix el teu operador", "calcula", options);
 			}catch(Exception e){
-				JOptionPane.showMessageDialog(null, "By!");
-				System.err.println(e.getMessage());
-				System.exit(0);
+				u.error("by!", e);
 			}
 			
 			m = "num2";
 			
 			try{
-				aux = JOptionPane.showInputDialog(null,m);
+				aux = u.input(m);
 				//y = Integer.parseInt(JOptionPane.showInputDialog(null, m));
 			}catch(Exception e){
-				JOptionPane.showMessageDialog(null, "Ale aw!");
-				System.err.println(e.getMessage());
-				System.exit(0);
+				u.error("Paca casa", e);
 			}
 			try{
-				y =Integer.parseInt(aux);
+				y = u.integer(aux);
 			}catch(NumberFormatException nfe){
-				JOptionPane.showMessageDialog(null, "No és un nombre!","Error",JOptionPane.ERROR_MESSAGE);
-				System.err.println(nfe.getMessage());
-				System.exit(0);
+				u.error("no es un nombre", nfe);
 			}
 		
 			// cridant al metode
@@ -89,11 +77,9 @@ public class Main {
 
 			m = "Vols realitzar un altra operació?";
 			try{
-				entrar = JOptionPane.showConfirmDialog(null, m);
+				entrar = u.confirm(m); 
 			}catch(Exception e){
-				JOptionPane.showMessageDialog(null, "By!");
-				System.err.println(e.getMessage());
-				System.exit(0);
+				u.error("BY!", e);
 			}
 			
 		}
